63d09808a125dbc235fe8d27313f3c38ea7649f8
Vincent <vincent01sj@gmail.com>
2022-09-26 08:47:17 +0700

    revisi UGD selesai

--- page/listMoviesPage.php
+++ page/listMoviesPage.php
@@ -1,7 +1,6 @@
 <?php
 include '../component/sidebar.php'?>
 
-    //penanda UGD Selesai buat commit, lupa dikasih keterangan kemarin.. :)
     <div class="container p-3 m-4 h-100" style="background-color: #FFFFFF; border-top: 5px solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" >
 
         <div class="body d-flex ju justify-content-between">
